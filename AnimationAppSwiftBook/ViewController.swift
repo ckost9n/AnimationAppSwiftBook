accaada3a522c50853c7467637aaee4d5aebee2f
ckost9n <ckost9n@gmail.com>
2021-09-10 18:33:09 +0300

    setup core animate and add Spring Framework

--- AnimationAppSwiftBook/ViewController.swift
+++ AnimationAppSwiftBook/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var coreAnimationView: UIView!
+    private var originCoordinate: CGFloat?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        originCoordinate = coreAnimationView.frame.origin.x
     }
 
-
+    @IBAction func startCoreAnimation(_ sender: UIButton) {
+        sender.pulsate()
+        
+        
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.autoreverse, .repeat], animations: {
+            if self.coreAnimationView.frame.origin.x == self.originCoordinate {
+                self.coreAnimationView.frame.origin.x -= 40
+            } else {
+                self.coreAnimationView.frame.origin.x += 40
+            }
+            
+        })
+    }
+    
 }
 
